b23b77cc222ba8ee569874444fdc6807da4d4a61
BilalIscar <Bilal@iscar.co.il>
2024-10-15 08:30:47 +0300

    Test Change 0

--- semiPrj/forms/Prices.aspx.vb
+++ semiPrj/forms/Prices.aspx.vb
@@ -143,6 +143,7 @@ Public Class Prices
 
     Private Sub GetAccountDetails()
         Try
+            Dim iiii As Int16 = 0
 
             lblCustomerNo.Text = StateManager.GetValue(StateManager.Keys.s_CustomerNumber, True)
             lblCustoName.Text = StateManager.GetValue(StateManager.Keys.s_CustomerName, True).ToString.Trim
